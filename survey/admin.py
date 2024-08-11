9dc2d6af02334308c6d7e933b783f73c381c1f80
ehsan malek <EhsanMalekBS@gmail.com>
2024-08-11 11:55:12 +0330

    register EmojiFeedback in admin.py

--- survey/admin.py
+++ survey/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import EmojiFeedback
+
 
 # Register your models here.
+
+@admin.register(EmojiFeedback)
+class EmojiFeedbackAdmin(admin.ModelAdmin):
+    list_display = ('emoji', 'count')
