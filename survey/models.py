80cbd5b5ac323aee2b34055fb1a8832fc13265af
ehsan malek <EhsanMalekBS@gmail.com>
2024-08-11 09:58:40 +0330

    create new model by name EmojiFeedback and makemigratons & migrate

--- survey/models.py
+++ survey/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class EmojiFeedback(models.Model):
+    EMOJI_CHOICES = [
+        ('😃', 'Happy'),
+        ('😐', 'Neutral'),
+        ('😞', 'Sad'),
+        ('😍', 'Love'),
+        ('😡', 'Angry'),
+    ]
+
+    emoji = models.CharField(max_length=1, choices=EMOJI_CHOICES)
+    count = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return  f'{self.emoji} - {self.count}'
