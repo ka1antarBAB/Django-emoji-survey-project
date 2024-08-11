f413bd7ce2a7f77680034822de8348f32434d264
ehsan malek <EhsanMalekBS@gmail.com>
2024-08-11 11:59:48 +0330

    create view for emoji_survey_view & emoji_survey_view in survey app

--- survey/views.py
+++ survey/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.admin.views.decorators import staff_member_required
+
+from survey.forms import EmojiSurveyForm
+from .models import EmojiFeedback
+
 
 # Create your views here.
+
+def emoji_survey_view(request):
+    if request.method == 'POST':
+        form = EmojiSurveyForm(request.POST)
+        if form.is_valid():
+            selected_emoji = form.cleaned_data['emoji']
+            feedback, created = EmojiFeedback.objects.get_or_create(emoji=selected_emoji)
+            feedback.count += 1
+            feedback.save()
+        else:
+            return form.errors
+    if request.method == 'GET':
+        form = EmojiSurveyForm()
+
+    return render(request, 'emoji_survey.html', {'form': form})
+
+
+@staff_member_required
+def survey_results_view(request):
+    feedbacks = EmojiFeedback.objects.all()
+    return render(request, 'survey_results.html', {'feedbacks': feedbacks})
