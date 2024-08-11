388db8334aa6373ebd9fb2eff5b341dce2106aac
ehsan malek <EhsanMalekBS@gmail.com>
2024-08-11 11:57:35 +0330

    change choices in EmojiFeedback model

--- survey/models.py
+++ survey/models.py
@@ -4,15 +4,15 @@ from django.db import models
 # Create your models here.
 class EmojiFeedback(models.Model):
     EMOJI_CHOICES = [
-        ('😃', 'Happy'),
-        ('😐', 'Neutral'),
-        ('😞', 'Sad'),
-        ('😍', 'Love'),
-        ('😡', 'Angry'),
+        ('Happy', '😃'),
+        ('Neutral', '😐'),
+        ('Sad', '😞'),
+        ('Love', '😍'),
+        ('Angry', '😡'),
     ]
 
-    emoji = models.CharField(max_length=1, choices=EMOJI_CHOICES)
+    emoji = models.CharField(max_length=10, choices=EMOJI_CHOICES)
     count = models.PositiveIntegerField(default=0)
 
     def __str__(self):
-        return  f'{self.emoji} - {self.count}'
+        return f'{self.emoji} - {self.count}'
